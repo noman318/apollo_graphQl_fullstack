b438020fbcb011d317489abc31b1bb192a766863
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-01-29 11:18:13 +0530

    Login and Register screen created

--- frontend/src/Screens/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
   return (
-    <Container>
+    <FormContainer>
       <h1>Login</h1>
-    </Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+      </Form>
+    </FormContainer>
   );
 };
 
--- frontend/src/Screens/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen.jsx
@@ -1,11 +1,45 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 const RegisterScreen = () => {
   return (
-    <Container>
+    <FormContainer>
       <h1>Sign Up</h1>
-    </Container>
+      <Form>
+        <Form.Group className="mb-2">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-2">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter First Name" />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Last Name" />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter User Name" />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Regsiter
+        </Button>
+      </Form>
+    </FormContainer>
   );
 };
 
