829064591b5aff2a216847e21a74810028305434
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-02-02 11:24:44 +0530

    Add project muatation integrated on frontend

--- frontend/src/components/AddProjectModal.jsx
+++ frontend/src/components/AddProjectModal.jsx
@@ -3,28 +3,50 @@ import { Modal, Form, Button } from "react-bootstrap";
 import { useMutation, useQuery } from "@apollo/client";
 import { toast } from "react-toastify";
 import { GoProjectRoadmap } from "react-icons/go";
+import { GET_ALL_CLIENTS } from "../queries/clientQueries";
+import { CREATE_NEW_PROJECT } from "../mutations/projectMutations";
 
 const AddProjectModal = () => {
   const initialForm = {
-    email: "",
     name: "",
-    phone: "",
+    status: "",
+    description: "",
+    clientId: "",
   };
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [formData, setFormData] = useState(initialForm);
-  const [clientId, setClientId] = useState("");
-
+  const { data } = useQuery(GET_ALL_CLIENTS);
+  const [createProject, { loading }] = useMutation(CREATE_NEW_PROJECT);
+  const { name, description, status, clientId } = formData;
+  const statusStrings = [
+    { key: "NotStarted", value: "Not Started" },
+    { key: "InProgress", value: "In Progress" },
+    { key: "Completed", value: "Completed" },
+  ];
   const handleAddNewClient = async (e) => {
     e.preventDefault();
     if (Object.values(formData).some((value) => value === "")) {
       toast.error("Fill all the fields to add new client");
-    }
-    try {
-    } catch (errors) {
-      console.log("errors", errors);
-      toast.error(`${errors}`);
+    } else {
+      try {
+        await createProject({
+          variables: {
+            input: {
+              name,
+              status,
+              description,
+              clientId,
+            },
+          },
+        });
+        toast.success("Client Added");
+        setFormData(initialForm);
+      } catch (errors) {
+        console.log("errors", errors);
+        toast.error(`${errors}`);
+      }
     }
   };
   const handleChange = (e) => {
@@ -33,15 +55,16 @@ const AddProjectModal = () => {
       [e.target.name]: e.target.value,
     });
   };
+  console.log("formData", formData);
   return (
     <React.Fragment>
       <Button variant="dark" className="mx-3" onClick={handleShow}>
         <GoProjectRoadmap style={{ marginRight: "10px" }} />
-        Add new Project
+        New Project
       </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Client</Modal.Title>
+          <Modal.Title>Project</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleAddNewClient}>
@@ -49,24 +72,45 @@ const AddProjectModal = () => {
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
+                name="name"
                 placeholder="Enter name"
                 onChange={handleChange}
               />
             </Form.Group>
-            <Form.Label>User</Form.Label>
-            <Form.Select onChange={(e) => setClientId(e.target.value)}>
-              <option>User</option>
-              {/* {data?.getUsers?.map((data, _) => (
-                <React.Fragment key={data?.id}>
-                  <option value={data?.id}>
-                    {data.firstName} {data.lastName}
-                  </option>
-                </React.Fragment>
-              ))} */}
-            </Form.Select>
+            <Form.Group className="mb-3">
+              <Form.Label>Status</Form.Label>
+              <Form.Select
+                // onChange={(e) => setStatus(e.target.value)}
+                onChange={handleChange}
+                name="status"
+              >
+                <option>Status</option>
+                {statusStrings?.map((data) => (
+                  <React.Fragment key={data.key}>
+                    <option value={data.key}>{data.value}</option>
+                  </React.Fragment>
+                ))}
+              </Form.Select>
+            </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+                as="textarea"
+                name="description"
+                rows={3}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Client</Form.Label>
+              <Form.Select onChange={handleChange} name="clientId">
+                <option>Client</option>
+                {data?.getAllClients?.map((data, _) => (
+                  <React.Fragment key={data?.id}>
+                    <option value={data?.id}>{data.name}</option>
+                  </React.Fragment>
+                ))}
+              </Form.Select>
             </Form.Group>
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose}>
@@ -75,7 +119,7 @@ const AddProjectModal = () => {
               <Button
                 variant="primary"
                 onClick={handleClose}
-                // disabled={loading}
+                disabled={loading}
                 type="submit"
               >
                 Add
--- frontend/src/mutations/projectMutations.js
+++ frontend/src/mutations/projectMutations.js
@@ -7,6 +7,7 @@ const CREATE_NEW_PROJECT = gql`
       name
       status
       description
+      clientId
     }
   }
 `;
